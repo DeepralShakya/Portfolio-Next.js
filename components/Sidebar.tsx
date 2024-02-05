2a83f1d42694835a1e3d5658502088e4d9319755
DeepralShakya <shakyadeepral44@gmail.com>
2024-02-05 12:48:57 +0545

    App Created

---
+++ components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVhtFJc9v3hEBfhJiOhYMS_60ieEbiOjPJyxl8F2dIBw&s" alt="Profile Image"/>
+      <h2>Deepral Shakya</h2>
+      <p>Frontend Developer</p>
+      <p>Download Resume</p>
+    </div>
+  )
+}
+
+export default Sidebar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14">
+      <div className="col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl"><Sidebar/></div>
+      <div className="col-span-12 bg-white lg:col-span-9 rounded-2xl">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
