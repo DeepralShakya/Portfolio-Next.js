4cfa58dec26764b9d008c925a02b49adb4fed2af
DeepralShakya <shakyadeepral44@gmail.com>
2024-04-09 19:32:07 +0545

    Image and Portfolio adjustment

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { FaGithub, FaCloudDownloadAlt,  FaInstagram, FaLinkedin } from "react-icons/fa";
 import { CiLocationOn } from "react-icons/ci";
 import { MdEmail } from "react-icons/md";
+import Link from 'next/link';
 
 const Sidebar = () => {
   return (
     <div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVhtFJc9v3hEBfhJiOhYMS_60ieEbiOjPJyxl8F2dIBw&s" 
-      className='mx-auto rounded-full h-30 w-30' alt="Profile Image"/>
+      <Link href="/DeepralProfile.png">
+        <img src="/DeepralProfile.png" className='object-cover object-top mx-auto rounded-full w-36 h-36' alt="Profile Image"/>
+      </Link>
       <h2 className='my-5 text-2xl font-bold'>Deepral Shakya</h2>
       <p className='p-2 bg-orange-100 rounded-full'>Frontend Developer</p>
-      <a className='flex items-center justify-center p-2 my-3 bg-orange-100 rounded-full' href='/assets/Frontend.pdf' download="Frontend.pdf"><FaCloudDownloadAlt className='w-6 h-6 mx-1'/>Download Resume</a>
+      <a className='flex items-center justify-center p-2 my-3 bg-orange-100 rounded-full' href='/assets/DeepralResume.pdf' download="DeepralResume.pdf"><FaCloudDownloadAlt className='w-6 h-6 mx-1'/>Download Resume</a>
       
       {/* icons */}
       <div className='flex justify-around mx-auto my-6'>
